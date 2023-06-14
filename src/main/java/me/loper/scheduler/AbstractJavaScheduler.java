328d32c5a919b24ff270c1b5e1158fdd92ff0594
loper <bizrenay@gmail.com>
2023-06-14 22:21:42 +0300

    update AbstractJavaScheduler & add Logger with base logger interface

--- src/main/java/me/loper/scheduler/AbstractJavaScheduler.java
+++ src/main/java/me/loper/scheduler/AbstractJavaScheduler.java
@@ -1,31 +1,36 @@
 package me.loper.scheduler;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import lombok.NonNull;
+import me.loper.logger.Logger;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * Abstract implementation of {@link SchedulerAdapter} using a {@link ScheduledExecutorService}.
  */
 public abstract class AbstractJavaScheduler implements SchedulerAdapter {
-    private final ScheduledThreadPoolExecutor scheduler;
-    private final ErrorReportingExecutor schedulerWorkerPool;
-    private final ForkJoinPool worker;
+    private static final int PARALLELISM = 16;
 
-    public AbstractJavaScheduler(String schedulerName) {
-        this.scheduler = new ScheduledThreadPoolExecutor(1, new ThreadFactoryBuilder()
-                .setDaemon(true)
-                .setNameFormat(schedulerName)
-                .build()
-        );
+    private final ScheduledThreadPoolExecutor scheduler;
+    private final ForkJoinPool worker;
+    private final Logger logger;
+    private final String schedulerName;
+
+    public AbstractJavaScheduler(String schedulerName, Logger logger) {
+        this.scheduler = new ScheduledThreadPoolExecutor(1, r -> {
+            Thread thread = Executors.defaultThreadFactory().newThread(r);
+            thread.setName(schedulerName);
+            return thread;
+        });
+        this.logger = logger;
+        this.schedulerName = schedulerName;
         this.scheduler.setRemoveOnCancelPolicy(true);
-        this.schedulerWorkerPool = new ErrorReportingExecutor(Executors.newCachedThreadPool(new ThreadFactoryBuilder()
-                .setDaemon(true)
-                .setNameFormat(schedulerName + "-worker-%d")
-                .build()
-        ));
-        this.worker = new ForkJoinPool(32, ForkJoinPool.defaultForkJoinWorkerThreadFactory, (t, e) -> e.printStackTrace(), false);
+        this.scheduler.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+        this.worker = new ForkJoinPool(PARALLELISM, new WorkerThreadFactory(schedulerName), new ExceptionHandler(), false);
     }
 
     @Override
@@ -35,13 +40,13 @@ public abstract class AbstractJavaScheduler implements SchedulerAdapter {
 
     @Override
     public SchedulerTask asyncLater(Runnable task, long delay, TimeUnit unit) {
-        ScheduledFuture<?> future = this.scheduler.schedule(() -> this.schedulerWorkerPool.execute(task), delay, unit);
+        ScheduledFuture<?> future = this.scheduler.schedule(() -> this.worker.execute(task), delay, unit);
         return () -> future.cancel(false);
     }
 
     @Override
     public SchedulerTask asyncRepeating(Runnable task, long interval, TimeUnit unit) {
-        ScheduledFuture<?> future = this.scheduler.scheduleAtFixedRate(() -> this.schedulerWorkerPool.execute(task), interval, interval, unit);
+        ScheduledFuture<?> future = this.scheduler.scheduleAtFixedRate(() -> this.worker.execute(task), interval, interval, unit);
         return () -> future.cancel(false);
     }
 
@@ -49,7 +54,9 @@ public abstract class AbstractJavaScheduler implements SchedulerAdapter {
     public void shutdownScheduler() {
         this.scheduler.shutdown();
         try {
-            this.scheduler.awaitTermination(1, TimeUnit.MINUTES);
+            if (!this.scheduler.awaitTermination(1, TimeUnit.MINUTES)) {
+                reportRunningTasks(thread -> thread.getName().equals(this.schedulerName));
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -57,41 +64,48 @@ public abstract class AbstractJavaScheduler implements SchedulerAdapter {
 
     @Override
     public void shutdownExecutor() {
-        this.schedulerWorkerPool.delegate.shutdown();
+        this.worker.shutdown();
         try {
-            this.schedulerWorkerPool.delegate.awaitTermination(1, TimeUnit.MINUTES);
+            if (!this.worker.awaitTermination(1, TimeUnit.MINUTES)) {
+                this.logger.severe("Timed out waiting for the " + this.schedulerName + " worker thread pool to terminate");
+                reportRunningTasks(thread -> thread.getName().startsWith(schedulerName + "-"));
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private static final class ErrorReportingExecutor implements Executor {
-        private final ExecutorService delegate;
+    private void reportRunningTasks(Predicate<Thread> predicate) {
+        Thread.getAllStackTraces().forEach((thread, stack) -> {
+            if (predicate.test(thread)) {
+                this.logger.warn("Thread " + thread.getName() + " is blocked, and may be the reason for the slow shutdown!\n" +
+                        Arrays.stream(stack).map(el -> "  " + el).collect(Collectors.joining("\n"))
+                );
+            }
+        });
+    }
 
-        private ErrorReportingExecutor(ExecutorService delegate) {
-            this.delegate = delegate;
+    private static final class WorkerThreadFactory implements ForkJoinPool.ForkJoinWorkerThreadFactory {
+        private static final AtomicInteger COUNT = new AtomicInteger(0);
+        private final String threadName;
+
+        WorkerThreadFactory(String threadName) {
+            this.threadName = threadName;
         }
 
         @Override
-        public void execute(@NonNull Runnable command) {
-            this.delegate.execute(new ErrorReportingRunnable(command));
+        public ForkJoinWorkerThread newThread(ForkJoinPool pool) {
+            ForkJoinWorkerThread thread = ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(pool);
+            thread.setDaemon(true);
+            thread.setName(this.threadName + "-" + COUNT.getAndIncrement());
+            return thread;
         }
     }
 
-    private static final class ErrorReportingRunnable implements Runnable {
-        private final Runnable delegate;
-
-        private ErrorReportingRunnable(Runnable delegate) {
-            this.delegate = delegate;
-        }
-
+    private final class ExceptionHandler implements Thread.UncaughtExceptionHandler {
         @Override
-        public void run() {
-            try {
-                this.delegate.run();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        public void uncaughtException(Thread t, Throwable e) {
+            AbstractJavaScheduler.this.logger.warn("Thread " + t.getName() + " threw an uncaught exception", e);
         }
     }
 }
